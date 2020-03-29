7eafea744c68a349e0adbac30f38ed5523a0a958
mindgamesnl pc <matsmoolhuizen@gmail.com>
2020-03-29 13:16:10 +0200

    shit

--- proxy/src/main/java/net/md_5/bungee/command/CommandBungee.java
+++ proxy/src/main/java/net/md_5/bungee/command/CommandBungee.java
@@ -16,6 +16,6 @@ public class CommandBungee extends Command
     @Override
     public void execute(CommandSender sender, String[] args)
     {
-        sender.sendMessage( ChatColor.BLUE + "This server is running BungeeCord version " + ProxyServer.getInstance().getVersion() + " by md_5" );
+        sender.sendMessage( ChatColor.BLUE + "This server is running the BarelyAuthenticated proxy (https://github.com/Mindgamesnl/BarelyAuthenticated) by Mindgamesnl, a redis authentication fork of BungeeCord by md_5" );
     }
 }
